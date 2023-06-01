5f90bc3f088e5eaa8d6ec2808572e51dabb20b0e
Pavel Burya <pavel.burya2@ibm.com>
2023-06-01 13:46:32 +0200

    added section 33 demo code

---
+++ section33-node/app.ts
@@ -0,0 +1,45 @@
+const input1Element = document.getElementById('input1') as HTMLInputElement;
+const input2Element = document.getElementById('input2') as HTMLInputElement;
+const buttonElement = document.getElementById('button') as HTMLButtonElement;
+
+type NumOrString = number | string;
+interface Result {
+  value: NumOrString;
+  date: Date;
+}
+
+// const sumResults: number[] = [];
+const sumResults: Array<number> = [];
+
+function add(num1: NumOrString, num2: NumOrString) {
+  if (typeof num1 === 'number' && typeof num2 === 'number') return num1 + num2;
+  if (typeof num1 === 'string' && typeof num2 === 'string')
+    return num1 + '_' + num2;
+  return +num1 + +num2;
+}
+
+function printResult(result: Result) {
+  console.log(result.value);
+}
+
+buttonElement?.addEventListener('click', () => {
+  const value1 = +input1Element?.value;
+  const value2 = +input2Element?.value;
+  const sum = add(value1, value2);
+  const sumResult = {
+    value: sum,
+    date: new Date(),
+  };
+  printResult(sumResult);
+  sumResults.push(sum as number);
+});
+
+const myPromise = new Promise<string>((res, _rej) => {
+  setTimeout(() => {
+    res('Hello world!');
+  });
+});
+
+myPromise.then((result) => {
+  console.log(result);
+});
