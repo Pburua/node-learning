22d71e9d36dd21933ed9ef9afdf90d64d8a0c8a6
Pavel Burya <pavel.burya2@ibm.com>
2023-06-01 13:43:59 +0200

    added generics

--- section33/app.js
+++ section33/app.js
@@ -1,22 +1,35 @@
-var input1Element = document.getElementById("input1");
-var input2Element = document.getElementById("input2");
-var buttonElement = document.getElementById("button");
+"use strict";
+const input1Element = document.getElementById('input1');
+const input2Element = document.getElementById('input2');
+const buttonElement = document.getElementById('button');
+// const sumResults: number[] = [];
+const sumResults = [];
 function add(num1, num2) {
-    if (typeof num1 === "number" && typeof num2 === "number")
+    if (typeof num1 === 'number' && typeof num2 === 'number')
         return num1 + num2;
-    if (typeof num1 === "string" && typeof num2 === "string")
+    if (typeof num1 === 'string' && typeof num2 === 'string')
         return num1 + '_' + num2;
     return +num1 + +num2;
 }
 function printResult(result) {
     console.log(result.value);
 }
-buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener('click', function () {
-    var value1 = +(input1Element === null || input1Element === void 0 ? void 0 : input1Element.value);
-    var value2 = +(input2Element === null || input2Element === void 0 ? void 0 : input2Element.value);
-    var sum = add(value1, value2);
-    printResult({
+buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener('click', () => {
+    const value1 = +(input1Element === null || input1Element === void 0 ? void 0 : input1Element.value);
+    const value2 = +(input2Element === null || input2Element === void 0 ? void 0 : input2Element.value);
+    const sum = add(value1, value2);
+    const sumResult = {
         value: sum,
         date: new Date(),
+    };
+    printResult(sumResult);
+    sumResults.push(sum);
+});
+const myPromise = new Promise((res, _rej) => {
+    setTimeout(() => {
+        res('Hello world!');
     });
+});
+myPromise.then((result) => {
+    console.log(result);
 });
--- section33/app.ts
+++ section33/app.ts
@@ -8,7 +8,8 @@ interface Result {
   date: Date;
 }
 
-const sumResults: number[] = [];
+// const sumResults: number[] = [];
+const sumResults: Array<number> = [];
 
 function add(num1: NumOrString, num2: NumOrString) {
   if (typeof num1 === 'number' && typeof num2 === 'number') return num1 + num2;
@@ -25,8 +26,20 @@ buttonElement?.addEventListener('click', () => {
   const value1 = +input1Element?.value;
   const value2 = +input2Element?.value;
   const sum = add(value1, value2);
-  printResult({
+  const sumResult = {
     value: sum,
     date: new Date(),
+  };
+  printResult(sumResult);
+  sumResults.push(sum as number);
+});
+
+const myPromise = new Promise<string>((res, _rej) => {
+  setTimeout(() => {
+    res('Hello world!');
   });
+});
+
+myPromise.then((result) => {
+  console.log(result);
 });
