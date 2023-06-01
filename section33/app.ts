173132ef9a732c295ab9c0da536dae6da1d42d7a
Pavel Burya <pavel.burya2@ibm.com>
2023-06-01 13:15:31 +0200

    added eslint, prettier, husky configs

--- section33/app.ts
+++ section33/app.ts
@@ -1,15 +1,18 @@
-
-const input1Element = document.getElementById("input1") as HTMLInputElement;
-const input2Element = document.getElementById("input2") as HTMLInputElement;
-const buttonElement = document.getElementById("button") as HTMLButtonElement;
+const input1Element = document.getElementById('input1') as HTMLInputElement;
+const input2Element = document.getElementById('input2') as HTMLInputElement;
+const buttonElement = document.getElementById('button') as HTMLButtonElement;
 
 type NumOrString = number | string;
-interface Result { value: NumOrString, date: Date }
+interface Result {
+  value: NumOrString;
+  date: Date;
+}
+
+const sumResults: number[] = [];
 
 function add(num1: NumOrString, num2: NumOrString) {
-  if (typeof num1 === "number" && typeof num2 === "number")
-    return num1 + num2;
-  if (typeof num1 === "string" && typeof num2 === "string")
+  if (typeof num1 === 'number' && typeof num2 === 'number') return num1 + num2;
+  if (typeof num1 === 'string' && typeof num2 === 'string')
     return num1 + '_' + num2;
   return +num1 + +num2;
 }
@@ -19,11 +22,11 @@ function printResult(result: Result) {
 }
 
 buttonElement?.addEventListener('click', () => {
-  const value1 = +input1Element?.value;
+  const value1 = +input1Element?.value
   const value2 = +input2Element?.value;
   const sum = add(value1, value2);
   printResult({
     value: sum,
     date: new Date(),
   });
-})
+});
