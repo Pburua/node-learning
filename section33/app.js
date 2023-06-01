968eadb5d5d85eb03c77ba4f1cfaa3bffa234352
Pavel Burya <pavel.burya2@ibm.com>
2023-06-01 11:47:30 +0200

    implemented summarize functionality

--- section33/app.js
+++ section33/app.js
@@ -1,5 +1,11 @@
+var input1Element = document.getElementById("input1");
+var input2Element = document.getElementById("input2");
+var buttonElement = document.getElementById("button");
 function add(num1, num2) {
     return num1 + num2;
 }
-console.log(add(1, 6));
-console.log(add('1', '6'));
+buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener('click', function () {
+    var value1 = +(input1Element === null || input1Element === void 0 ? void 0 : input1Element.value);
+    var value2 = +(input2Element === null || input2Element === void 0 ? void 0 : input2Element.value);
+    console.log(add(value1, value2));
+});
--- section33/app.ts
+++ section33/app.ts
@@ -1,7 +1,14 @@
-function add(num1, num2) {
+
+const input1Element = document.getElementById("input1") as HTMLInputElement;
+const input2Element = document.getElementById("input2") as HTMLInputElement;
+const buttonElement = document.getElementById("button") as HTMLButtonElement;
+
+function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-console.log(add(1, 6));
-
-console.log(add('1', '6'));
+buttonElement?.addEventListener('click', () => {
+  const value1 = +input1Element?.value
+  const value2 = +input2Element?.value
+  console.log(add(value1, value2));
+})
