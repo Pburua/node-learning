6d4ce6f5a412f7f959144842a78af82f0e1742e3
Pavel Burya <pavel.burya2@ibm.com>
2023-06-01 12:39:06 +0200

    added type and interface definitions

--- section33/app.js
+++ section33/app.js
@@ -2,10 +2,21 @@ var input1Element = document.getElementById("input1");
 var input2Element = document.getElementById("input2");
 var buttonElement = document.getElementById("button");
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === "number" && typeof num2 === "number")
+        return num1 + num2;
+    if (typeof num1 === "string" && typeof num2 === "string")
+        return num1 + '_' + num2;
+    return +num1 + +num2;
+}
+function printResult(result) {
+    console.log(result.value);
 }
 buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener('click', function () {
     var value1 = +(input1Element === null || input1Element === void 0 ? void 0 : input1Element.value);
     var value2 = +(input2Element === null || input2Element === void 0 ? void 0 : input2Element.value);
-    console.log(add(value1, value2));
+    var sum = add(value1, value2);
+    printResult({
+        value: sum,
+        date: new Date(),
+    });
 });
--- section33/app.ts
+++ section33/app.ts
@@ -3,12 +3,27 @@ const input1Element = document.getElementById("input1") as HTMLInputElement;
 const input2Element = document.getElementById("input2") as HTMLInputElement;
 const buttonElement = document.getElementById("button") as HTMLButtonElement;
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
+type NumOrString = number | string;
+interface Result { value: NumOrString, date: Date }
+
+function add(num1: NumOrString, num2: NumOrString) {
+  if (typeof num1 === "number" && typeof num2 === "number")
+    return num1 + num2;
+  if (typeof num1 === "string" && typeof num2 === "string")
+    return num1 + '_' + num2;
+  return +num1 + +num2;
+}
+
+function printResult(result: Result) {
+  console.log(result.value);
 }
 
 buttonElement?.addEventListener('click', () => {
-  const value1 = +input1Element?.value
-  const value2 = +input2Element?.value
-  console.log(add(value1, value2));
+  const value1 = +input1Element?.value;
+  const value2 = +input2Element?.value;
+  const sum = add(value1, value2);
+  printResult({
+    value: sum,
+    date: new Date(),
+  });
 })
