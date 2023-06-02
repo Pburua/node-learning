9a1e62ba0f22877611ccb8c8e0cd892a782fea3a
Pavel Burya <pavel.burya2@ibm.com>
2023-06-02 15:01:17 +0200

    specified oak version

--- section35/deno/app.ts
+++ section35/deno/app.ts
@@ -1,4 +1,4 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/oak@v12.5.0/mod.ts";
 
 import todosRoutes from './routes/todos.ts';
 import { connect } from "./helpers/db.ts";
